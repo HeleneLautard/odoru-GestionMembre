9ee9e749e652cbe48059091fa0b970903526cf34
HeleneLautard <helene.lautard@toulouse.miage.fr>
2022-06-01 19:02:16 +0200

    Initial commit

---
+++ src/main/java/fr/miage/toulouse/m2/lautard/helene/odoru/gestionmembre/GestionMembreApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m2.lautard.helene.odoru.gestionmembre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMembreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionMembreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/toulouse/m2/lautard/helene/odoru/gestionmembre/GestionMembreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m2.lautard.helene.odoru.gestionmembre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMembreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
